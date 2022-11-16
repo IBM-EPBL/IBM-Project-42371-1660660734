a75b9a83fbe36d6d3cb64ab6be3e9bf34f55b3b2
Poornakala-G <98613133+Poornakala-G@users.noreply.github.com>
2022-11-16 16:10:45 +0530

    Add files via upload

---
+++ Assessments/M2  Lead/Assignment_4/app.py
@@ -0,0 +1,10 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+if __name__=='__main__':
+    app.run(port=5000, host="0.0.0.0", debug=True)
+
